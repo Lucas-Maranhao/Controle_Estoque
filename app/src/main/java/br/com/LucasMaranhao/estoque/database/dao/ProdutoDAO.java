765c6f40d221adc8b74186b85f0465c429e19eb9
Lucas Maranhão <Lucasvsm@gmail.com>
2022-06-15 03:13:52 -0300

    Criando um aplicativo de controle de estoque

---
+++ app/src/main/java/br/com/LucasMaranhao/estoque/database/dao/ProdutoDAO.java
@@ -0,0 +1,29 @@
+package br.com.LucasMaranhao.estoque.database.dao;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import br.com.LucasMaranhao.estoque.model.Produto;
+
+@Dao
+public interface ProdutoDAO {
+
+    @Insert
+    long salva(Produto produto);
+
+    @Update
+    void atualiza(Produto produto);
+
+    @Query("SELECT * FROM Produto")
+    List<Produto> buscaTodos();
+
+    @Query("SELECT * FROM Produto WHERE id = :id")
+    Produto buscaProduto(long id);
+
+    @Delete
+    void remove(Produto produto);
+}
---
+++ app/src/main/java/br/com/LucasMaranhao/estoque/retrofit/service/ProdutoService.java
@@ -0,0 +1,13 @@
+package br.com.LucasMaranhao.estoque.retrofit.service;
+
+import java.util.List;
+
+import br.com.LucasMaranhao.estoque.model.Produto;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ProdutoService {
+
+    @GET("produto")
+    Call<List<Produto>> buscaTodos();
+}
